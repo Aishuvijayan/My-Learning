6ce8adacf908fc563821cd6b7b5eddccf3f577b5
Aishuvijayan <41662492+Aishuvijayan@users.noreply.github.com>
2018-07-25 10:55:48 +0530

    Item-catalog micro service

---
+++ Item-Catalog/src/main/java/com/example/demo/ItemCatalogApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackageClasses=ItemCatalogApplicationController.class)
+@EnableDiscoveryClient
+public class ItemCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemCatalogApplication.class, args);
+	}
+}
